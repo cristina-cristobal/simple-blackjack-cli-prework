e008009d7f257d31ad093d49707b589b500a6eec
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-22 19:19:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,11 +48,13 @@ end
 
 def runner
   welcome
-   hand=initial_round
-   until hand>21 do
-     hand=hit?(hand)
-     display_card_total(hand)
-   end
-   end_game(hand)
- end	
+  initial_round
+  prompt_user
+  hit? 
+    if input == "s"
+      prompt_user
+  end
+  if card_total > 21
+  end_game
+end
 end
